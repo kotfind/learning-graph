ee82f182927a87011f14be29ead7c1ecff65d20c
kotfind <kotfind@yandex.ru>
2022-07-05 23:29:39 +0300

    FOREIGN KEY in gb

--- src/logics/WorkerCore.cpp
+++ src/logics/WorkerCore.cpp
@@ -28,10 +28,17 @@ WorkerCore* WorkerCore::getInstance() {
 void WorkerCore::initDb() {
     QSqlQuery query;
 
+    query.prepare("CREATE TABLE IF NOT EXISTS packages("
+        // id is rowid
+        "name VARCHAR(255) NOT NULL UNIQUE"
+        ")");
+    query.exec();
+    query.finish();
+
     query.prepare("CREATE TABLE IF NOT EXISTS themes("
         // id is rowid
         "name VARCHAR(255) NOT NULL,"
-        "package_id INT NOT NULL,"
+        "package_id INT NOT NULL REFERENCES pacakges(rowid),"
         "description TEXT,"
         "in_wishlist INT CHECK(in_wishlist in (0, 1)),"
         "is_learned INT CHECK(is_learned in (0, 1)),"
@@ -42,16 +49,9 @@ void WorkerCore::initDb() {
 
     query.prepare("CREATE TABLE IF NOT EXISTS theme_dependencies("
         // id is rowid
-        "from_id INT NOT NULL,"
-        "to_id INT NOT NULL,"
+        "from_id INT NOT NULL REFERENCES themes(rowid),"
+        "to_id INT NOT NULL REFERENCES themes(rowid),"
         "UNIQUE (from_id, to_id)"
-        ")");
-    query.exec();
-    query.finish();
-
-    query.prepare("CREATE TABLE IF NOT EXISTS packages("
-        // id is rowid
-        "name VARCHAR(255) NOT NULL UNIQUE"
         ")");
     query.exec();
     query.finish();
