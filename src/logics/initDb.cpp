0e66124b0a791d74f2c922cb2d5ab350444b034e
kotfind <kotfind@yandex.ru>
2022-07-06 00:13:18 +0300

    Fix ee82f18 (FOREIGN KEY in gb)

--- src/logics/initDb.cpp
+++ src/logics/initDb.cpp
@@ -15,7 +15,7 @@ void WorkerCore::initDb() {
     query.prepare("CREATE TABLE IF NOT EXISTS themes("
         // id is rowid
         "name VARCHAR(255) NOT NULL,"
-        "package_id INT NOT NULL REFERENCES pacakges(rowid),"
+        "package_id INT NOT NULL REFERENCES packages(rowid),"
         "description TEXT,"
         "in_wishlist INT CHECK(in_wishlist in (0, 1)),"
         "is_learned INT CHECK(is_learned in (0, 1)),"
