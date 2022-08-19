c1421f125c3b682255c1bfb7f6002926bb2bc70c
kotfind <kotfind@yandex.ru>
2022-08-19 12:11:27 +0300

    Redesign graph editor

--- src/GraphEditTab.cpp
+++ src/GraphEditTab.cpp
@@ -9,8 +9,8 @@
 #include <QButtonGroup>
 #include <QCheckBox>
 #include <QIcon>
-#include <QSlider>
 #include <QLabel>
+#include <QSpinBox>
 
 GraphEditTab::GraphEditTab(QWidget* parent)
         : QMainWindow(parent) {
@@ -27,33 +27,49 @@ void GraphEditTab::ui() {
 }
 
 void GraphEditTab::uiHeader() {
-    auto* toolbar = addToolBar("Graph Toolbar");
-    auto* toolBtns = new QButtonGroup(this);
+    // Mode Bar
+    auto* modeBar = addToolBar("Mode Bar");
+    auto* modeBtns = new QButtonGroup(this);
 
     auto* pointerBtn = new QToolButton;
     pointerBtn->setIcon(QIcon(":pointer.svg"));
     pointerBtn->setCheckable(true);
     pointerBtn->setChecked(true);
-    toolBtns->addButton(pointerBtn);
-    toolbar->addWidget(pointerBtn);
+    modeBtns->addButton(pointerBtn);
+    modeBar->addWidget(pointerBtn);
 
     auto* arrowBtn = new QToolButton;
     arrowBtn->setIcon(QIcon(":arrow.svg"));
     arrowBtn->setCheckable(true);
-    toolBtns->addButton(arrowBtn);
-    toolbar->addWidget(arrowBtn);
+    modeBtns->addButton(arrowBtn);
+    modeBar->addWidget(arrowBtn);
 
     auto* newNodeBtn = new QToolButton;
     newNodeBtn->setIcon(QIcon(":plus1.svg"));
     newNodeBtn->setCheckable(true);
-    toolBtns->addButton(newNodeBtn);
-    toolbar->addWidget(newNodeBtn);
+    modeBtns->addButton(newNodeBtn);
+    modeBar->addWidget(newNodeBtn);
 
     auto* addNodeBtn = new QToolButton;
     addNodeBtn->setIcon(QIcon(":plus2.svg"));
     addNodeBtn->setCheckable(true);
-    toolBtns->addButton(addNodeBtn);
-    toolbar->addWidget(addNodeBtn);
+    modeBtns->addButton(addNodeBtn);
+    modeBar->addWidget(addNodeBtn);
+
+    // Settings Bar
+    auto* settingsBar = addToolBar("Settings Bar");
+
+    auto* depthLabel = new QLabel("Depth:");
+    settingsBar->addWidget(depthLabel);
+
+    auto* depthSpin = new QSpinBox;
+    depthSpin->setRange(5, 500);
+    settingsBar->addWidget(depthSpin);
+
+    settingsBar->addSeparator();
+
+    auto* learnedCheckbox = new QCheckBox(tr("Show Learned"));
+    settingsBar->addWidget(learnedCheckbox);
 }
 
 void GraphEditTab::uiBody() {
@@ -75,31 +91,6 @@ void GraphEditTab::uiFooter() {
     bodyVBox->addLayout(hbox);
 
     hbox->addStretch(1);
-
-    // Depth Slider
-    auto* depthLabel = new QLabel("Depth:");
-    hbox->addWidget(depthLabel);
-
-    auto* depthSlider = new QSlider(Qt::Horizontal);
-    depthSlider->setMinimumWidth(200);
-    depthSlider->setRange(5, 100);
-    hbox->addWidget(depthSlider);
-
-    auto* depthNumLabel = new QLabel;
-    connect(
-        depthSlider,
-        &QSlider::valueChanged,
-        [depthNumLabel](int val) {
-            depthNumLabel->setText(QString("(%1)").arg(val, 3));
-        }
-    );
-    hbox->addWidget(depthNumLabel);
-    depthSlider->setValue(5);
-    emit depthSlider->valueChanged(5); // idkn last line won't do it
-
-    // Learned Checkbox
-    auto* learnedCheckbox = new QCheckBox(tr("Show Learned Themes"));
-    hbox->addWidget(learnedCheckbox);
 
     // Export Button
     auto* exportBtn = new QPushButton("Export");
