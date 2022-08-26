51d3cb2304a51ca61244c888c0c7fb26e2e6effc
kotfind <kotfind@yandex.ru>
2022-08-26 10:22:17 +0300

    SmartListWidet in PackageTab

--- src/PackageTab.cpp
+++ src/PackageTab.cpp
@@ -1,7 +1,6 @@
 #include "PackageTab.h"
 
 #include "sqlDefines.h"
-#include "PackageListWidget.h"
 #include "GlobalSignalHandler.h"
 #include "PackageInfoDialog.h"
 
@@ -10,6 +9,7 @@
 #include <QPushButton>
 #include <QInputDialog>
 #include <QMessageBox>
+#include <QMenu>
 
 PackageTab::PackageTab(QWidget* parent)
         : QWidget(parent) {
@@ -17,10 +17,47 @@ PackageTab::PackageTab(QWidget* parent)
 
     connect(
         this,
+        &PackageTab::themesUpdated,
+        GlobalSignalHandler::getInstance(),
+        &GlobalSignalHandler::themesUpdated
+    );
+
+    connect(
+        this,
         &PackageTab::packagesUpdated,
         GlobalSignalHandler::getInstance(),
         &GlobalSignalHandler::packagesUpdated
     );
+
+    connect(
+        GlobalSignalHandler::getInstance(),
+        &GlobalSignalHandler::packagesUpdated,
+        this,
+        &PackageTab::update
+    );
+
+    connect(
+        GlobalSignalHandler::getInstance(),
+        &GlobalSignalHandler::themesUpdated,
+        this,
+        &PackageTab::update
+    );
+
+    connect(
+        packagesList,
+        &SmartListWidget::menuRequested,
+        this,
+        &PackageTab::packageMenuRequested
+    );
+
+    connect(
+        packagesList,
+        &SmartListWidget::doubleClicked,
+        this,
+        &PackageTab::packageDoubleClicked
+    );
+
+    update();
 }
 
 void PackageTab::ui() {
@@ -42,12 +79,87 @@ void PackageTab::ui() {
     hbox->addWidget(importBtn);
 
     // Packages List
-    auto* packagesList = new PackageListWidget;
-    packagesList->update();
+    packagesList = new SmartListWidget;
     vbox->addWidget(packagesList);
 }
 
 void PackageTab::onCreateBtn() {
     PackageInfoDialog d(-1, this);
     d.exec();
+}
+
+void PackageTab::update() {
+    PREPARE_NEW(query, " \
+        SELECT p.id, p.name, ( \
+            SELECT COUNT(*) \
+            FROM themes t \
+            WHERE t.packageId = p.id \
+        ) \
+        FROM packages p \
+        ORDER BY p.name \
+    ")
+    EXEC(query)
+
+    packagesList->clear();
+    while (query.next()) {
+        packagesList->addItem(
+            tr("%1 (%2 themes)")
+                .arg(query.value(1).toString())
+                .arg(query.value(2).toInt()),
+            query.value(0).toInt()
+        );
+    }
+}
+
+void PackageTab::packageDoubleClicked(int packageId) {
+    PackageInfoDialog d(
+        packageId,
+        this
+    );
+    d.exec();
+}
+
+void PackageTab::packageMenuRequested(int packageId, const QPoint& globalPos) {
+    QMenu menu;
+
+    menu.addAction(tr("Rename"), [=]() {
+        PackageInfoDialog d(
+            packageId,
+            this
+        );
+        d.exec();
+    });
+
+    menu.addAction(tr("Delete"), [=]() {
+        if (QMessageBox::question(
+                this,
+                "Question",
+                tr("Delete package \"%1\"?").arg(packageId)) // TODO packageId -> packageName
+                    == QMessageBox::Yes) {
+
+            QSqlQuery query;
+
+            PREPARE(query, " \
+                DELETE \
+                FROM packages \
+                WHERE id = ? \
+            ")
+            query.addBindValue(packageId);
+            EXEC(query)
+            query.finish();
+
+            PREPARE(query, " \
+                DELETE \
+                FROM themes \
+                WHERE packageId = ? \
+            ")
+            query.addBindValue(packageId);
+            EXEC(query)
+
+            emit packagesUpdated();
+            emit themesUpdated();
+        }
+    });
+
+    menu.exec(globalPos);
 }
--- src/PackageTab.h
+++ src/PackageTab.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "SmartListWidget.h"
+
 #include <QWidget>
 
 class PackageTab : public QWidget {
@@ -11,9 +13,17 @@ class PackageTab : public QWidget {
     private:
         void ui();
 
+        SmartListWidget* packagesList;
+
     signals:
         void packagesUpdated();
+        void themesUpdated();
+
+    public slots:
+        void update();
 
     private slots:
         void onCreateBtn();
+        void packageDoubleClicked(int packageId);
+        void packageMenuRequested(int packageId, const QPoint& globalPos);
 };
