6a1576a7801225b1bb6d30bdb6a259c42046a839
kotfind <kotfind@yandex.ru>
2022-11-01 16:09:06 +0300

    Comment functions in db/db.h

--- src/db/db.h
+++ src/db/db.h
@@ -52,11 +52,25 @@ namespace db {
     };
 
     namespace theme {
+        // Returns name of theme with id id
         QString name(int id);
+
+        // Returns name of package of theme with id id
         QString packageName(int id);
+
+        // Returns Theme structure for theme with id id
         Theme read(int id);
 
-        // Selects themes with current options
+        // Returns list of themes with current filters
+        // Theme name is LIKE (in SQL terms) %name%
+        // Theme packageId equals packageId
+        //     or can be any if packageId == -1
+        // Theme should be in wishlist if inWishlist == Qt::Checked,
+        //     should not be in wishlist if inWishlist == Qt::Unchecked,
+        //     can be or not be in wishlist if inWishlist == Qt::PartlyChecked
+        // Theme should be learned if isLearned == Qt::Checked,
+        //     should not be learned if isLearned == Qt::Unchecked,
+        //     can be or not be learned if isLearned == Qt::PartlyChecked
         // XXX: No description selected
         QList<Theme> reads(
             const QString& name,
@@ -65,76 +79,118 @@ namespace db {
             Qt::CheckState isLearned
         );
 
-        // Selects themes that are not
-        // presenred on current graph
+        // Returns list of themes which are not
+        // presenred on graph with id excludeGraphId
         // XXX: No description selected
-        QList<Theme> reads(int excludeGraph);
+        QList<Theme> reads(int excludeGraphId);
 
-        // Selects themes with current ids
+        // Selects themes with ids ids
         // XXX: description is selected
         QList<Theme> reads(const QList<int>& ids);
 
-        // Inserts theme if id == -1; updates otherwise
-        // Return value is id
+        // Inserts theme into db if id == -1,
+        //    updates existant theme otherwise
+        // Returns theme's id
         int write(const Theme&);
 
+        // Deletes theme with id id
         void del(int id);
 
+        // Exports themes with ids ids into file "filename" as txt
         void exportAsTxt(const QString& filename, const QList<int>& ids);
 
+        // Returns true if theme with id id exists in db
+        // Return false otherwise
         bool exists(int id);
     };
 
     namespace package {
+        // Returns name of package with id id
         QString name(int id);
+
+        // Returns quantity of themes in package with id id
         int count(int id);
-        Package read(int);
+
+        // Returns Package structure for package with id id
+        Package read(int id);
+
+        // Returns list of packages with
+        // package name LIKE (in SQL terms) %name%
         QList<Package> reads(const QString& name);
 
-        // Inserts theme if id == -1; updates otherwise
-        // Return value is id
+        // Inserts package into db if id == -1,
+        //    updates existant package otherwise
+        // Returns package's id
         int write(const Package&);
 
+        // Deletes package with id id
         void del(int id);
 
-        // returns ids of themes from multiple packages
+        // Returns ids of themes from packages with ids packageIds
         QList<int> getThemeIds(const QList<int>& packageIds);
 
+        // Exports packages with ids ids into file "filename" as txt
         void exportAsTxt(const QString& filename, const QList<int>& ids);
     };
 
     namespace graph {
+        // Returns name of graph with id id
         QString name(int id);
+
+        // Returns quantity of themes in graph with id id
         int count(int id);
+
+        // Returns Graph structure for graph with id id
         Graph read(int);
+
+        // Returns list of graphs with
+        // graph name LIKE (in SQL terms) %name%
         QList<Graph> reads(const QString& name);
 
-        // Inserts theme if id == -1; updates otherwise
-        // Return value is id
+        // Inserts graph into db if id == -1,
+        //    updates existant graph otherwise
+        // Returns graph's id
         int write(const Graph&);
 
+        // Deletes graph with id id
         void del(int id);
 
+        // Returns true if graph with id id exists in db
+        // Return false otherwise
         bool exists(int id);
     };
 
     namespace graphNode {
+        // Returns GraphNode structure for graphNode with id id
         GraphNode read(int id);
+
+        // Return graphNodes presented on graph with id graphId
         QList<GraphNode> reads(int graphId);
 
-        int create(const GraphNode&);
+        // Inserts graphNode n into db
+        int create(const GraphNode& n);
 
+        // Sets position of graphNode with id id to (x, y)
         void move(int id, double x, double y);
 
+        // Deletes graphNode with id id
         void del(int id);
     };
 
     namespace themeEdge {
-        // Set themeId = -1 for any themeId
+        // Returns themeEdges presented on graph with id graphId
+        // If themeId != -1, than returns only edges
+        //     than begin or end in theme with id themeId
         QList<ThemeEdge> reads(int graphId, int themeId);
 
+        // Inserts edge
+        //     from node with id beginNodeId
+        //     to   node with id endNodeId
+        // into db
+        // Returns edge's is
         int create(int beginNodeId, int endNodeId);
 
+        // Deletes edge with id edgeId
         void del(int edgeId);
     };
 };
