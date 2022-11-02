0d8170c5b29aaa1863e3e3a5d1d17f90bf322066
kotfind <kotfind@yandex.ru>
2022-11-03 00:01:53 +0300

    Try-catch and test group name in testDefines.h

--- src/tests/testDefines.h
+++ src/tests/testDefines.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <QDebug>
+#include <QString>
 
 int __TEST_NUMBER__;
 int __TESTS_PASSED__;
 int __TESTS_FAILED__;
+QString __TEST_GROUP_NAME__;
 
 #define TEST_GROUP_BEGIN(name) \
+    __TEST_GROUP_NAME__ = (name); \
     __TEST_NUMBER__ = 1; \
     __TESTS_PASSED__ = 0; \
     __TESTS_FAILED__ = 0; \
@@ -15,7 +18,7 @@ int __TESTS_FAILED__;
 
 #define TEST_PASSED \
     qDebug().noquote() << \
-        QString("Test %1: ok").arg(__TEST_NUMBER__);\
+        QString("Test %1: pass").arg(__TEST_NUMBER__);\
     ++__TEST_NUMBER__; \
     ++__TESTS_PASSED__;
 
@@ -26,17 +29,22 @@ int __TESTS_FAILED__;
     ++__TESTS_FAILED__;
 
 #define TEST_CHECK(v) \
-    if (v) { \
-        TEST_PASSED \
-    } else { \
+    try { \
+        if (v) { \
+            TEST_PASSED \
+        } else { \
+            TEST_FAILED \
+        } \
+    } catch (...) { \
         TEST_FAILED \
-    } \
+    }
 
 #define TEST_COMPARE(a, b) \
     TEST_CHECK((a) == (b))
 
 #define TEST_GROUP_END \
     qDebug().noquote() << \
-        QString("---------- Done (Passed: %1, Failed: %2) ----------\n") \
+        QString("Tested %1: (Passed: %2, Failed: %3)\n") \
+            .arg(__TEST_GROUP_NAME__) \
             .arg(__TESTS_PASSED__) \
             .arg(__TESTS_FAILED__);
