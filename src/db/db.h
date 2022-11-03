a10893e73a345f573695c99270f29bea28b9c6a2
kotfind <kotfind@yandex.ru>
2022-11-03 13:51:12 +0300

    Move structure defenitions to sepparate file

--- src/db/db.h
+++ src/db/db.h
@@ -1,55 +1,12 @@
 #pragma once
 
+#include "../types.h"
+
 #include <QString>
 #include <QList>
 
 namespace db {
     bool init();
-
-    struct Package {
-        Package();
-
-        int id;
-        QString name;
-        int count;
-    };
-
-    struct Theme {
-        Theme();
-
-        int id;
-        QString name;
-        Package package;
-        bool inWishlist;
-        bool isLearned;
-        QString description;
-    };
-
-    struct Graph {
-        Graph();
-
-        int id;
-        QString name;
-        int count;
-    };
-
-    struct GraphNode {
-        GraphNode();
-
-        int id;
-        int graphId;
-        int themeId;
-        double x;
-        double y;
-    };
-
-    struct ThemeEdge {
-        ThemeEdge();
-
-        int id;
-        int beginId;
-        int endId;
-    };
 
     namespace theme {
         // Returns name of theme with id id
