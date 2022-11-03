6c1ba57a577c8001a25e5d67d4997137c08e1ca1
kotfind <kotfind@yandex.ru>
2022-11-03 19:09:19 +0300

    Comment defines

--- src/db/sqlDefines.h
+++ src/db/sqlDefines.h
@@ -4,12 +4,15 @@
 #include <QDebug>
 #include <QSqlError>
 
+// Prints debug information about failed query query
 #define LOG_FAILED_QUERY(query) \
     qDebug().noquote() << QString("QUERY FAILED at %1(%2)")\
         .arg(__FILE__) \
         .arg(__LINE__) \
         << (query).lastError();
 
+// Prepares query query from code str
+// Prints debug info if failed
 #define PREPARE(query, str) \
 if (!(query).prepare(str)) { \
     qDebug().noquote() << QString("PREPARE FAILED at %1(%2)")\
@@ -20,11 +23,14 @@ if (!(query).prepare(str)) { \
 } \
 (query).setForwardOnly(true);
 
+// Creates and prepares query query from code str
+// Prints debug info if failed
 #define PREPARE_NEW(query, str) \
 QSqlQuery query; \
 PREPARE(query, str)
 
-
+// Executes query query
+// Prints debug info if failed
 #define EXEC(query) \
 if (!(query).exec()) { \
     qDebug().noquote() << QString("EXEC FAILED at %1(%2)")\
@@ -34,8 +40,10 @@ if (!(query).exec()) { \
     throw 0; \
 }
 
+// SQL error codes
 #define SQLITE_CONSTRAINT_UNIQUE 2067
 #define SQLITE_CONSTRAINT_NOTNULL 1299
 #define SQLITE_CONSTRAINT_CHECK 275
 
+// Returns error code of query query
 #define ERR_CODE(query) (query).lastError().nativeErrorCode().toInt()
